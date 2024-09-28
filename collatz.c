80e2deba22b19e401a5e8c45d5229d2c687480ee
Nathan2113 <warnernathan2001@gmail.com>
2024-09-28 11:02:18 -0700

    Implemented LFU and LRU command line support

--- collatz.c
+++ collatz.c
@@ -2,33 +2,46 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <ctype.h>
 
 unsigned collatz_r(__uint64_t candidate);
 
+float cache_wrapper(char* cache_method, __uint64_t candidate);
+
 int main(int argc, char* argv[]) {
-    if(argc < 3) {
-        printf("[-] Must pass the number of values to test (N), the smallest value to test (MIN) and the largest value to test (MAX)\n");
-        printf("[-] EXAMPLE: ./collatz 15 1 100\n");
+    if(argc < 5) {
+        printf("[-] Must pass the number of values to test (N), the smallest value to test (MIN), the largest value to test (MAX) and the caching method (LRU, LFU)\n");
+        printf("[-] EXAMPLE: ./collatz 15 1 100 LRU\n");
+        return 0;
     } // END if(argc < 3)
 
-    // THIS WILL NEED TO BE CHANGED LATER
-    if(atoi(argv[1]) < 1) {
-        printf("[-] Must pass a value greater than 0\n");
+    if(atoi(argv[1]) < 1 || atoi(argv[2]) < 1 || atoi(argv[3]) < 1) {
+        printf("[-] Must pass a value greater than 0 for all integer inputs\n");
+        printf("[-] EXAMPLE: ./collatz 15 1 100 LRU\n");
+        return 0;
+    } // END if(atoi(argv[1]) < 1 || atoi(argv[2]) < 1 || atoi(argv[3]) < 1)
+
+    // Initializing the string for what cache method to be used, then checking if it's valid
+    char* cache_method = argv[4];
+
+    // Converting command line argument for cache method to all uppercase
+    for(int i = 0; i < sizeof(cache_method); i++) {
+        cache_method[i] = toupper(cache_method[i]);
+    } // END for(int i = 0; i < sizeof(cache_method); i++)
+
+    if(strcmp(cache_method, "LRU") != 0 && strcmp(cache_method, "LFU") != 0) {
+        printf("[-] Invalid cache method...Use either \"LRU\" or \"LFU\"\n");
+        printf("[-] EXAMPLE: ./collatz 15 1 100 LRU\n");
+
+        return 0;
     }
+
 
     srand(time(NULL));
 
     FILE* fptr;
     fptr = fopen("results.csv", "w");
-
-    // In order to get random number in a range
-        // rand() % (max_number + 1 - minimum_number) + minimum_number
-
-    // TESTING
-    // printf("Min Value: %u\n", atoi(argv[2]));
-    // printf("Max Value: %u\n", atoi(argv[3]));
-
-    // printf("Value passed: %lu\n", collatz_candidate);
 
     int step_count = 0;
     __uint64_t min = atoi(argv[2]);
@@ -36,16 +49,32 @@ int main(int argc, char* argv[]) {
 
     fprintf(fptr, "Collatz Candidate, Step Count\n");
 
+
     for(int i = 0; i < atoi(argv[1]); i++) {
         __uint64_t collatz_candidate = rand() % (max - min + 1) + min;
 
-        step_count = collatz_r(collatz_candidate);
+        // step_count = collatz_r(collatz_candidate);
+        step_count = cache_wrapper(cache_method, collatz_candidate);
 
         printf("Step count for %lu: %u\n", collatz_candidate, step_count);
         fprintf(fptr, "%lu,%u\n", collatz_candidate, step_count);
     }
 
     return 0;
+}
+
+float cache_wrapper(char* cache_method, __uint64_t candidate) {
+    // TESTING
+    // if(strcmp(cache_method, "LRU")) {
+    //     printf("You are using %s\n", cache_method);
+    // }
+    // else if(strcmp(cache_method, "LFU")) {
+    //     printf("You are using %s\n", cache_method);
+    // }
+
+    int step_count = collatz_r(candidate);
+
+    return step_count;
 }
 
 unsigned collatz_r(__uint64_t candidate) {
